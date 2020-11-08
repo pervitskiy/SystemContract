578bc8002f951d2b31d57eaf966c3e6d1aad3778
Дмитрий Первицкий <dmitrijpervickij@MacBook-Pro-Dmitrij.local>
2020-11-08 16:46:29 +0300

    added javadoc class BubbleSort

--- src/main/java/sorter/BubbleSort.java
+++ src/main/java/sorter/BubbleSort.java
@@ -2,7 +2,19 @@ package sorter;
 
 import java.util.Comparator;
 
+/**
+ * BubbleSort
+ * @param <T> the type of objects to be sorted
+ */
 public class BubbleSort<T> implements ISorter<T> {
+    /**
+     * Sort Ascending method BubbleSort
+     *
+     * @param array - the array to sort
+     * @param comparator - comparison criterion
+     * @param start - start index of the array
+     * @param end - end index of the array
+     */
     @Override
     public void sort(T[] array, Comparator<T> comparator, int start, int end) {
         boolean sorted = false;
--- src/main/java/sorter/ISorter.java
+++ src/main/java/sorter/ISorter.java
@@ -3,7 +3,22 @@ package sorter;
 import java.util.Comparator;
 
 public interface ISorter<T> {
+     /**
+      * Sort Ascending
+      *
+      * @param array - the array to sort
+      * @param comparator - comparison criterion
+      * @param start - start index of the array
+      * @param end - end index of the array
+      */
      void sort(T[] array, Comparator<T> comparator, int start, int end);
+
+     /**
+      * Sort Ascending
+      *
+      * @param array - the array to sort
+      * @param comparator - comparison criterion
+      */
      default void sort(T[] array, Comparator<T> comparator){
           if (array.length > 1)
                sort(array, comparator, 0, array.length-1);
