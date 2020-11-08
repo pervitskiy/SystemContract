01e4f41f0b1656cd1e000568d3dab27f4439a578
Дмитрий Первицкий <dmitrijpervickij@MacBook-Pro-Dmitrij.local>
2020-11-08 17:44:12 +0300

    added class QuickSort

---
+++ src/main/java/sorter/QuickSort.java
@@ -0,0 +1,43 @@
+package sorter;
+
+import java.util.Comparator;
+
+public class QuickSort<T> implements ISorter<T> {
+
+    @Override
+    public void sort(T[] array, Comparator<T> comparator, int start, int end) {
+        if (start < end) {
+        /* pi is partitioning index, arr[pi] is now
+           at right place */
+            int pi = partition(array, comparator, start, end);
+            sort(array, comparator, start, pi - 1);  // Before pi
+            sort(array, comparator, pi + 1, end); // After pi
+        }
+    }
+
+    int partition(T arr[],Comparator<T> comparator, int low, int high)
+    {
+        T pivot = arr[high];
+        int i = (low-1); // index of smaller element
+        for (int j=low; j<high; j++)
+        {
+            // If current element is smaller than the pivot
+            if (comparator.compare(arr[j],pivot) < 0)
+            {
+                i++;
+
+                // swap arr[i] and arr[j]
+                T temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        // swap arr[i+1] and arr[high] (or pivot)
+        T temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+
+        return i+1;
+    }
+
+}
